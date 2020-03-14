097f75020d861710d3bb1ee5cb78bb5d8f2c0f16
Cancercookie <jaritarabelli1@icloud.com>
2020-03-14 19:22:35 +0100

    Added UI and BGMusic
    action needed for the UI blockout

---
+++ Assets/Scripts/BackgroundMusicLoop.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundMusicLoop : MonoBehaviour
+{
+    public AudioSource myAudio;
+    [Range(0f, 26.5f)] // loops at 14.45-23.17
+    public float loopStart, loopEnd;
+ 
+    void Start(){
+        myAudio.Play();
+    }
+ 
+
+    void Update(){
+        // TODO: put here
+        if (myAudio.isPlaying && myAudio.time > loopEnd) {
+            myAudio.time = loopStart;
+        }
+    }
+}
