56e29e199bdbf5c3a1fb24f16a480af983dfa24c
Pier Carlo <imparatopiercarlo@gmail.com>
2020-03-15 14:38:31 +0100

    Score Logics and Score UI

---
+++ Assets/Scripts/PlayerManager.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerManager : MonoBehaviour
+{
+    public int score = 0;
+    public int life = 0;
+    public bool isGameOver;
+
+    // UI
+    public Text playerScore_txt;
+
+    // EVENTS
+    public delegate void GameOver();
+    public event GameOver onGameOver;
+
+    // STATS
+    private const int MAX_PLAYER_LIFE = 3;
+
+    public static PlayerManager instance = null;
+
+    private void Awake()
+    {
+        if (!instance)
+        {
+            instance = this;
+        }
+    }
+
+    void Start()
+    {
+        score = 0;
+        life = MAX_PLAYER_LIFE;
+        playerScore_txt.text = score.ToString();
+    }
+
+    public void RemoveLife(int amount = 1)
+    {
+        int tmpLife = life - amount;
+
+        if (tmpLife <= 0 && !isGameOver)
+        {
+            isGameOver = true;
+            onGameOver?.Invoke(); // SEND GAME OVER EVENT
+            tmpLife = 0;
+            return;
+        }
+
+        life = tmpLife;
+    }
+
+    public void AddScore(int amount)
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+
+        int tmpScore = score + amount;
+        if (tmpScore >= int.MaxValue)
+        {
+            score = int.MaxValue;
+            playerScore_txt.text = tmpScore.ToString();
+            return;
+        }
+        score = tmpScore;
+        playerScore_txt.text = tmpScore.ToString();
+    }
+}
--- Assets/Scripts/ThrowableObject.cs
+++ Assets/Scripts/ThrowableObject.cs
@@ -4,6 +4,13 @@ public class ThrowableObject : MonoBehaviour
 {
     private Rigidbody rb;
     private float throwForce;
+    public bool isThrowing;
+    private bool hasCollide;
+    private float timer = 0.0f;
+    private const float MIN_TIME_ON_COLLISION = 0.5f;
+
+    [Header("Score Info:")]
+    public int tableScore = 100;
     void Start()
     {
         rb = GetComponent<Rigidbody>();
@@ -12,15 +19,27 @@ public class ThrowableObject : MonoBehaviour
 
     public void Throw(float _throwForce)
     {
+        isThrowing = true;
         rb.transform.parent = null;
         rb.isKinematic = false;
         throwForce = _throwForce;
         rb.AddForce(Camera.main.transform.forward * throwForce, ForceMode.Impulse);
     }
-
-    private void OnCollisionEnter(Collision collision)
+    private void OnCollisionStay(Collision collision)
     {
-        Debug.Log(collision.collider.name);
+        isThrowing = false;
+        
+        Debug.Log(collision.transform.name);
         Destroy(gameObject, 2.0f);
+        timer += Time.deltaTime;
+        if (!hasCollide && timer >= MIN_TIME_ON_COLLISION)
+        {
+            if (collision.transform.CompareTag("Table"))
+            {
+                hasCollide = true;
+                PlayerManager.instance.AddScore(tableScore);
+                return;
+            }
+        }
     }
 }
